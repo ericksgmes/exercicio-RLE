53f6e3d546fa40416d4b51f3a812ea9eb34398f9
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-02-26 15:36:04 -0300

    REFACTOR: read_file

--- main.c
+++ main.c
@@ -2,78 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct 
+typedef struct 
 {
-    char *type;
-    int numColunas;
-    int numLinhas;
+    char type[2];
+    int numColumns;
+    int numRows;
     int **mat;
 } t_pgmImage;
 
 void RLE(char *input, int length, FILE *outputFile) {
 }
 
-int PDMtoTXT(const char *inputfile, const char *outputfile) {
-    FILE * inputFileName = fopen(inputfile, "r");
-    if (inputFileName == NULL) {
-        printf("Error opening input file");
+int **allocMatrix( int rows, int columns)
+{
+    int **mat = (int**) malloc(sizeof(int*) * rows);
+    for (int i=0; i<rows; ++i)
+    {
+    mat[i] = (int*)malloc(sizeof(int) * columns);
+    }
+    return mat;
+}
+
+void freeMatrix(int **mat, int columns)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        free(mat[i]);
+    }
+    free(mat);
+}
+
+int read_file(const char *input_file)
+ {
+    FILE * inputFileName = fopen(input_file, "r+");
+    if(inputFileName == NULL)
+    {
+        printf("Failed to load input file");
         return 0;
     }
 
-    FILE *outputFileName = fopen(outputfile, "wb");
-    if (outputFileName == NULL) {
-        printf("Error opening output file");
-        return 0;
-    }
+    t_pgmImage *image_info = (t_pgmImage *) malloc(sizeof(t_pgmImage));
+    fscanf(inputFileName, "%c", &image_info[0].type[0]);
+    fscanf(inputFileName, "%c", &image_info[0].type[1]);
+    fscanf(inputFileName, "%d", &image_info[0].numColumns);
+    fscanf(inputFileName, "%d", &image_info[0].numRows);
+    char buffer[3];
+    fscanf(inputFileName, "%2s", buffer);
 
-    int height, width, maxval, aux;
-    char buffer[MAX_LINE_LENGTH];
-    aux=0;
-    while (fgets(buffer, MAX_LINE_LENGTH, inputFileName))
-    { 
-        aux++;
-        if(aux > 3){
-            fprintf(outputFileName, "%s", buffer);
+    image_info->mat = allocMatrix(image_info->numRows, image_info->numColumns);
+
+    for(int i = 0; i < image_info->numRows ; i++) {
+        for(int j = 0; j < image_info->numColumns; j++) {
+            fscanf(inputFileName, "%d", &image_info->mat[i][j]);
         }
     }
-
-    fclose(inputFileName);
-    fclose(outputFileName);
+    
     return 1;
-}
-
-int TXTtoRLE(const char* TxtFileName) {
-        FILE *inputFile = fopen(TxtFileName, "r");
-    if (inputFile == NULL) {
-        printf("Error opening input file.\n");
-        return 0;
-    }
-
-    FILE *outputFile = fopen("output23.txt", "w");
-    if (outputFile == NULL) {
-        printf("Error opening output file.\n");
-        fclose(inputFile);
-        return 0;
-    }
-
-    char buffer[MAX_LINE_LENGTH];
-    while (fgets(buffer, MAX_LINE_LENGTH, inputFile)) {
-        int actual_lenght = strlen(buffer);
-        RLE(buffer, actual_lenght, outputFile);
-    }
-
-    fclose(inputFile);
-    fclose(outputFile);
-    return 1;
-}
+ }
 
 int main() {
     const char *input_filename = "ex1.pgm";
     const char *output_filename = "output.txt";
 
-    PDMtoTXT(input_filename, output_filename);
-    TXTtoRLE(output_filename);
-    printf("Conversion successful.\n");
+    PGMtoTXT(input_filename);
+    // TXTtoRLE(output_filename);
+    
 
     return 0;
 }
