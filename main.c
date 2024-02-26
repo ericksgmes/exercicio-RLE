47067fb26ab3cc68486701c7e8c28837d196291a
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-02-26 11:54:55 -0300

    adding struct def

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINE_LENGTH 1000
+
+struct 
+{
+    char *type;
+    int numColunas;
+    int numLinhas;
+    int **mat;
+} t_pgmImage;
 
 void RLE(char *input, int length, FILE *outputFile) {
 }
