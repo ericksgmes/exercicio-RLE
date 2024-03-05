1d852ca5e5e5fa3325ba3104c93c0b3413a62783
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-03-05 20:02:42 -0300

    style(saving-before-branching): WIP file_decompression and RLE_encoding

--- main.c
+++ main.c
@@ -95,8 +95,49 @@ int file_compression(const char *to_RLE_file, const char* output_RLE_file)
     return 1;
  }
 
-int file_decompression(const char *to_PGM_file, const char* output_PGM_file) {
-    FILE * inputFileName = fopen(to_PGM_file, "r+");
+int **RLE_decoding(const char *output_file, t_pgmImage * image){
+    FILE * outputFileName = fopen(output_file, "w");
+    if(outputFileName == NULL)
+    {
+        printf("Failed to load output file");
+        return 0;
+    }
+
+    fprintf(outputFileName, "P2\n");
+    fprintf(outputFileName, "%d ", image->numColumns);
+    fprintf(outputFileName, "%d\n", image->numRows);
+    fprintf(outputFileName, "%d\n", image->maxGray);
+    int *line = (int*) malloc(sizeof(int*) * image->numColumns);
+    memset(line, 0, sizeof(int) * image->numColumns);
+    int currentValue, count;
+    int j=0;
+    for (int i = 0; i < image->numRows; i++) {
+            currentValue = image->mat[i][j]; 
+            count = 1; 
+            for (j = 1; j < image->numColumns; j++) {
+                if (image->mat[i][j] == currentValue) {
+                    count++;
+                } else {
+                    for (int k = 0; k < count; k++) {
+                        fprintf(outputFileName, "%d ", currentValue);
+                    }
+                    currentValue = image->mat[i][j];
+                    count = 1;
+                }
+            }
+            for (int k = 0; k < count; k++) {
+                fprintf(outputFileName, "%d ", currentValue);
+            }
+            fprintf(outputFileName, "\n");
+        }
+    fclose(outputFileName);
+    free(line);
+    return image->mat;
+}
+
+int file_decompression(const char *to_RLE_file, const char* output_RLE_file)
+ {
+    FILE * inputFileName = fopen(to_RLE_file, "r+");
     if(inputFileName == NULL)
     {
         printf("Failed to load input file");
@@ -110,39 +151,24 @@ int file_decompression(const char *to_PGM_file, const char* output_PGM_file) {
     fscanf(inputFileName, "%d", &image_info->maxGray);
 
     image_info->mat = allocMatrix(image_info->numRows, image_info->numColumns, sizeof(int));
-    
-    for(int i = 0; i < image_info->numRows ; i++) {
-        for(int j = 0; j < image_info->numColumns; j++) {
-            char pixel;
-            fscanf(inputFileName, " %c", &pixel);
-            if(pixel == '@') fscanf(inputFileName, "%d", &image_info->mat[i][j]);
-        }
-    }
-    
-    printf("%s\n", image_info->type);
-    printf("%d\n", image_info->numColumns);
-    printf("%d ", image_info->numRows);
-    printf("%d\n", image_info->maxGray);
 
     for(int i = 0; i < image_info->numRows ; i++) {
         for(int j = 0; j < image_info->numColumns; j++) {
-            int d = image_info->mat[i][j];
-            printf("%d", d);
+            fscanf(inputFileName, "%d", &image_info->mat[i][j]);
         }
     }
-
-    //RLE_encoding(output_PGM_file, image_info);
+    RLE_decoding(output_RLE_file, image_info);
     fclose(inputFileName);
     freeMatrix((void **)image_info->mat, image_info->numRows);
     return 1;
-}
+ }
 
 int main() {
 // int argc, char *argv[]
 
     //file_compression(argv[1], argv[2]);
-    file_compression("ex2.pgm", "TEST.pgmc");
-    //file_decompression("TEST.pgmc", "out21.pgm");
+    file_compression("ex1.pgm", "TEST.pgmc");
+    file_decompression("TEST.pgmc", "out78.txt");
 
     return 0;
 }
